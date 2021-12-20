98a89530fdc54ec82a332a32be0cd1882014e451
guobeibei <guobeibei@tal.com>
2021-12-20 21:12:31 +0800

    如何动态配置环境变量

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # webpack-use
 webpack基本配置
+## 核心概念
+- webpack的默认配置文件是`webpack.config.js`
+- 入口：指示webpack应该从哪个文件开始打包，用来作为内部依赖图构建的起点
+    - 在webpack5里面如果没有配置，默认入口文件是src/index.js
+- loader
+    - webpack在默认情况下只能处理js和json文件
+    - 要想引入其他类型的文件，比如css文件，需要对原文件进行加载和转译，转成js
+    - 比如处理css文件['style-loader','css-loader'],从右向左执行
+    - 1. 先读取源文件index.css
+    - 2. 把文件内容传给css-loader，css-loader可以处理css中的@import和url语法，处理完之后会把内容传递给style-loader
+    - 3. style-loader的作用是把css转换成style标签插入到页面里
+- 插件：插件是一个类，通过new产生实例，new的时候可传入参数
+- mode，代表当前编译的环境：none未指定， production生产环境，webpack会针对构建结果进行生产环境的优化，比如压缩文件，development开发环境，不会进行压缩
+    - 如何动态设置不同的环境
+    -  --mode用来设置模块内（根据入口编译的js模块）的process.env.NODE_ENV
+         - webpack会在编译的时候把process.env.NODE_ENV替换成环境字符串,在生产环境中没有process.env.NODE_ENV变量
+    -  --env用来设置webpack配置文件的函数参数
+        - 在webpack.config.js文件中，配置时判断环境，进行动态配置
+    -  cross-env(跨平台设置环境变量)用来设置node环境的process.env.NODE_ENV
+        - cross-env NODE_ENV=xxx 相当于设置了node环境变量，改变了process.env.NODE_ENV
+    -  DefinePlugin用来设置模块内的全局变量,相当于进行模块内的字符串替换
+    - mode的优先级：默认是production < 配置文件（webpack.config.js中的mode）< package中的命令传参
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack')
+// module.exports = {
+//     // mode: 'development',  // none  development production
+//     devtool: 'source-map',
+//     // 指定项目打包的入口
+//     entry: './src/index.js',
+//     output: { // 指定文件输出的目录，默认是dist，输出目录必须是一个绝对路径
+//         path: path.resolve(__dirname, 'dist'),
+//         filename: "main.js"
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.css$/,
+//                 use:['style-loader', 'css-loader']
+//             }
+//         ]
+//     },
+//     plugins: [
+//         new HtmlWebpackPlugin({
+//             template: "./src/index.html"
+//         })
+//     ]
+// }
+
+module.exports = function (env) {
+    console.log(env); // {production: true 或 development: true}
+    // 在src文件中可以使用process.env.NODE_ENV，表示当前打包时的node环境里的变量
+    // 在这里的时候，process指当前node进程 env代表环境变量 NODE_ENV代表一个key
+    // cross-env NODE_ENV=production相当于设置了node环境变量，改变了process.env.NODE_ENV
+    console.log(process.env.NODE_ENV, '环境变量'); //没有设置cross-env NODE_ENV=xxx时，为undefined
+    return {
+        mode: env.production ? 'production' : 'development',  // none  development production
+        devtool: 'source-map',
+        // 指定项目打包的入口
+        entry: './src/index.js',
+        output: { // 指定文件输出的目录，默认是dist，输出目录必须是一个绝对路径
+            path: path.resolve(__dirname, 'dist'),
+            filename: "main.js"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use:['style-loader', 'css-loader']
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./src/index.html"
+            }),
+            new webpack.DefinePlugin({
+                // JSON.stringify =》 '"' + process.env.NODE_ENV + '"'， 不加""相当于取的是变量，应该是字符串
+                'xxx': JSON.stringify(process.env.NODE_ENV), // 设置在模块内xxx = process.env.NODE_ENV
+            })
+        ]
+    }
+}
